db13f79e7ceb61d7ee039a1ff265747cd83e4ff2
Andrew Reifman <andrewr23@gmail.com>
2014-03-10 02:03:34 -0400

    added more animations and sign up page

--- js/main.js
+++ js/main.js
@@ -5,20 +5,50 @@ $(document).ready(function() {
   $.fn.fullpage({
     verticalCentered: true,
     css3: true,
-    slidesColor: ['#32b9b1', '#f15a5a', '#a6ce59', '#0fa6db', '#f19849'],
+    slidesColor: ['#32b9b1', '#f15a5a', '#a6ce59', '#0fa6db', '#ab65e6'],
     navigation: true,
     navigationPosition: 'right',
-    anchors: ['intro', 'problem', 'themes', 'drops', 'finished', 'signup'],
+    anchors: ['intro', 'problem', 'themes', 'drops', 'signup'],
+    menu: '#menu',
     navigationTooltips: ['Intro', 'Problem', 'Themes', 'Drops', 'Signup'],
     scrollingSpeed: 100,
-    fixedElements: '.shoe-small',
+    fixedElements: '.logo, .bottom-buttons',
+    onLeave: function(index, direction){
+      if(index == '1' && direction =='down'){
+        $('.logo .text').delay(800).animate({'margin-top': '-200px'}, 300, 'easeInBack');
+        $('.intro').addClass('intro-hidden');
+      }
+      if(index == '5' && direction =='up'){
+        $('.bottom-buttons').delay(800).animate({'bottom': '15px'}, 300, 'easeOutBack');
+      }
+    },
     afterLoad: function(anchorLink){
-      if(anchorLink === 'themes'){
+      if(anchorLink == 'intro'){
+        $('.logo .text').delay(800).animate({'margin-top': '0px'}, 200, 'easeOutBack');
+        $('.intro').removeClass('intro-hidden');
+      }
+      if(anchorLink == 'themes'){
         $('.theme-1').delay(1000).animate({'margin-left': '-420px'}, 300, 'easeOutBack');
         $('.theme-2').delay(1000).animate({'margin-left': '-350px'}, 300, 'easeOutBack');
         $('.theme-3').delay(1000).animate({'margin-left': '-150px'}, 300, 'easeOutBack');
         $('.theme-4').delay(1000).animate({'margin-left': '-80px'}, 300, 'easeOutBack');
       }
+      if(anchorLink == 'signup'){
+        $('.bottom-buttons').delay(600).animate({'bottom': '-100px'}, 300, 'easeInBack');
+      }
+    }
+  });
+
+
+  $('.logo').mouseenter(function(){
+    if($(".intro").is('.intro-hidden')){
+      $('.logo .text').animate({'margin-top': '0px'}, 200, 'easeOutBack');
+    }
+  });
+
+  $('.logo').mouseleave(function(){
+    if($(".intro").is('.intro-hidden')){
+      $('.logo .text').animate({'margin-top': '-200px'}, 300, 'easeInBack');
     }
   });
 
@@ -55,7 +85,7 @@ $(document).ready(function() {
         $(ui.draggable).detach().css({top: 0,left: 0}).appendTo(this);
         $(this).addClass('complete');
 
-        if ($('.browser-content .complete').length === 1) {
+        if ($('.browser-content .complete').length === 6) {
           $('.drops').addClass('completed');
         }
       }
